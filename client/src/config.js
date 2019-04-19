b067bcd623728f036286aed8f8a2bc049335e069
EC2 Default User <ec2-user@ip-172-31-58-150.ec2.internal>
2019-04-19 05:56:18 +0000

    change config file

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,7 @@
 var config = {    
-    url : 'https://aj0cogumb8.execute-api.us-east-1.amazonaws.com/dev/source'
+    url : 'http://localhost:3000/source'
 }
+
+//https://aj0cogumb8.execute-api.us-east-1.amazonaws.com/dev/source
 
 export default config
